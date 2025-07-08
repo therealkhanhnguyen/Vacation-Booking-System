dbce96a78901a0d6da042ccde73886d3dbcc81d1
kngu414 <kngu414@wgu.edu>
2025-07-07 18:23:25 -0700

    Task A Initial Spring Boot project setup

---
+++ src/main/java/com/khanhnguyend288/khanhnguyend288/Khanhnguyend288Application.java
@@ -0,0 +1,13 @@
+package com.khanhnguyend288.khanhnguyend288;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Khanhnguyend288Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Khanhnguyend288Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/khanhnguyend288/khanhnguyend288/Khanhnguyend288ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khanhnguyend288.khanhnguyend288;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Khanhnguyend288ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
