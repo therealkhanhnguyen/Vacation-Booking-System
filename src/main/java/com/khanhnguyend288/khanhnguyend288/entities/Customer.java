3e49ec2fd156bdb622b4219526558b13b275091b
kngu414 <kngu414@wgu.edu>
2025-07-09 12:32:45 -0700

    Task G: Created validation in Customer

--- src/main/java/com/khanhnguyend288/khanhnguyend288/entities/Customer.java
+++ src/main/java/com/khanhnguyend288/khanhnguyend288/entities/Customer.java
@@ -18,22 +18,22 @@ import java.util.Set;
 public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "customer_id")
+    @Column(name = "customer_id", nullable = false)
     private Long id;
 
-    @Column(name = "customer_first_name")
+    @Column(name = "customer_first_name", nullable = false)
     private String firstName;
 
-    @Column(name = "customer_last_name")
+    @Column(name = "customer_last_name", nullable = false)
     private String lastName;
 
-    @Column(name = "address")
+    @Column(name = "address", nullable = false)
     private String address;
 
-    @Column(name = "postal_code")
+    @Column(name = "postal_code", nullable = false)
     private String postalCode;
 
-    @Column(name = "phone")
+    @Column(name = "phone", nullable = false)
     private String phone;
 
     @Column(name = "create_date")
