a61667548e9cb8a5741b7ea9b3db37e1b16f6bba
kngu414 <kngu414@wgu.edu>
2025-07-16 13:14:13 -0700

    Debugged sample customers not showing.

--- src/main/java/com/khanhnguyend288/khanhnguyend288/UsersSamples/UserSamples.java
+++ src/main/java/com/khanhnguyend288/khanhnguyend288/UsersSamples/UserSamples.java
@@ -33,77 +33,77 @@ public class UserSamples implements CommandLineRunner {
         Division division4 = divisionRepository.findById(34L).orElse(null);
         Division division5 = divisionRepository.findById(35L).orElse(null);
 
+        if (customerRepository.count() < 5) {
+
+            Customer c1 = new Customer();
+            c1.setFirstName("Khanh");
+            c1.setLastName("Nguyen");
+            c1.setAddress("123 Test Street");
+            c1.setPhone("555-0000");
+            c1.setPostalCode("99999");
+            c1.setCreateDate(LocalDateTime.now());
+            c1.setLastUpdate(LocalDateTime.now());
+            c1.setDivision(division1);
+            c1.setCarts(new HashSet<>()); // Prevent null pointer
+            customerRepository.save(c1);
 
 
-        Customer c1 = new Customer();
-        c1.setFirstName("Khanh");
-        c1.setLastName("Nguyen");
-        c1.setAddress("123 Test Street");
-        c1.setPhone("555-0000");
-        c1.setPostalCode("99999");
-        c1.setCreateDate(LocalDateTime.now());
-        c1.setLastUpdate(LocalDateTime.now());
-        c1.setDivision(division1);
-        c1.setCarts(new HashSet<>()); // Prevent null pointer
-        customerRepository.save(c1);
+            Customer c2 = new Customer();
+            c2.setFirstName("Paolo");
+            c2.setLastName("Maldini");
+            c2.setAddress("Stadio SanSiro");
+            c2.setPhone("555-1111");
+            c2.setPostalCode("12345");
+            c2.setCreateDate(LocalDateTime.now());
+            c2.setLastUpdate(LocalDateTime.now());
+            c2.setDivision(division2);
+            c2.setCarts(new HashSet<>());
+            customerRepository.save(c2);
 
+            Customer c3 = new Customer();
+            c3.setFirstName("Ronaldo");
+            c3.setLastName("de Lima");
+            c3.setAddress("Estadio Bernabeu");
+            c3.setPhone("555-2222");
+            c3.setPostalCode("67890");
+            c3.setCreateDate(LocalDateTime.now());
+            c3.setLastUpdate(LocalDateTime.now());
+            c3.setDivision(division3);
+            c3.setCarts(new HashSet<>());
+            customerRepository.save(c3);
 
-        Customer c2 = new Customer();
-        c2.setFirstName("Paolo");
-        c2.setLastName("Maldini");
-        c2.setAddress("Stadio SanSiro");
-        c2.setPhone("555-1111");
-        c2.setPostalCode("12345");
-        c2.setCreateDate(LocalDateTime.now());
-        c2.setLastUpdate(LocalDateTime.now());
-        c2.setDivision(division2);
-        c2.setCarts(new HashSet<>());
-        customerRepository.save(c2);
+            Customer c4 = new Customer();
+            c4.setFirstName("Leo");
+            c4.setLastName("Messi");
+            c4.setAddress("Estadio Camp Nou");
+            c4.setPhone("555-3333");
+            c4.setPostalCode("54321");
+            c4.setCreateDate(LocalDateTime.now());
+            c4.setLastUpdate(LocalDateTime.now());
+            c4.setDivision(division4);
+            c4.setCarts(new HashSet<>());
+            customerRepository.save(c4);
 
-        Customer c3 = new Customer();
-        c3.setFirstName("Ronaldo");
-        c3.setLastName("de Lima");
-        c3.setAddress("Estadio Bernabeu");
-        c3.setPhone("555-2222");
-        c3.setPostalCode("67890");
-        c3.setCreateDate(LocalDateTime.now());
-        c3.setLastUpdate(LocalDateTime.now());
-        c3.setDivision(division3);
-        c3.setCarts(new HashSet<>());
-        customerRepository.save(c3);
+            Customer c5 = new Customer();
+            c5.setFirstName("Thierry");
+            c5.setLastName("Henry");
+            c5.setAddress("Emirates Stadium");
+            c5.setPhone("555-4444");
+            c5.setPostalCode("11223");
+            c5.setCreateDate(LocalDateTime.now());
+            c5.setLastUpdate(LocalDateTime.now());
+            c5.setDivision(division5);
+            c5.setCarts(new HashSet<>());
+            customerRepository.save(c5);
 
-        Customer c4 = new Customer();
-        c4.setFirstName("Leo");
-        c4.setLastName("Messi");
-        c4.setAddress("Estadio Camp Nou");
-        c4.setPhone("555-3333");
-        c4.setPostalCode("54321");
-        c4.setCreateDate(LocalDateTime.now());
-        c4.setLastUpdate(LocalDateTime.now());
-        c4.setDivision(division4);
-        c4.setCarts(new HashSet<>());
-        customerRepository.save(c4);
+            // Verify inserts
+            List<Customer> customers = customerRepository.findAll();
+            for (Customer c : customers) {
+                System.out.println(c.getFirstName() + " " + c.getLastName());
+            }
 
-        Customer c5 = new Customer();
-        c5.setFirstName("Thierry");
-        c5.setLastName("Henry");
-        c5.setAddress("Emirates Stadium");
-        c5.setPhone("555-4444");
-        c5.setPostalCode("11223");
-        c5.setCreateDate(LocalDateTime.now());
-        c5.setLastUpdate(LocalDateTime.now());
-        c5.setDivision(division5);
-        c5.setCarts(new HashSet<>());
-        customerRepository.save(c5);
+            System.out.println("Inserted customers successfully.");
 
-        // Verify inserts
-        List<Customer> customers = customerRepository.findAll();
-        for (Customer c : customers) {
-            System.out.println(c.getFirstName() + " " + c.getLastName());
         }
-
-        System.out.println("Inserted customers successfully.");
-
-
     }
 }
